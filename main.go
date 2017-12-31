e57f75476035366fc8b56fa0e6b61d925d8c3d94
Unknown <goncharovnikita.dev@gmail.com>
2017-12-31 16:06:40 +0300

    Add random image method supported by flag

--- main.go
+++ main.go
@@ -1,48 +1,44 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 	"os/exec"
-	"os/user"
 	"path/filepath"
 )
 
-var dailyImage []byte
-var bingAPI BingAPI
+var randomFlag = flag.Bool("r", false, "set random picture as wallpaper")
+var cacheDirname = "/.wallpaperize_cache"
 
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 }
 
 func main() {
+	flag.Parse()
+	if *randomFlag != false {
+		setRandomPhoto()
+		return
+	}
 	setPhotoOfTheDay()
 }
 
 func setPhotoOfTheDay() {
 	var (
 		tempFilename = "/bing_daily_image.png"
-		cacheDirname = "/.wallpaperize_cache"
-		usr          *user.User
 		absPath      string
 		err          error
 		file         *os.File
+		dailyImage   []byte
+		bingAPI      BingAPI
 	)
+
+	createCacheFolder()
 
 	dailyImage = bingAPI.GetDailyImage()
 
-	if usr, err = user.Current(); err != nil {
-		log.Fatal(err)
-	}
-
-	if err = os.Mkdir(usr.HomeDir+cacheDirname, 0777); err != nil {
-		var ok bool
-		if _, ok = err.(*os.PathError); !ok {
-			log.Fatal(err)
-		}
-	}
-
-	if file, err = os.OpenFile(usr.HomeDir+cacheDirname+tempFilename, os.O_CREATE|os.O_RDWR, 0666); err != nil {
+	if file, err = os.OpenFile(getAbsCacheDirname()+tempFilename, os.O_CREATE|os.O_RDWR, 0666); err != nil {
 		log.Fatal(err)
 	}
 
@@ -52,7 +48,40 @@ func setPhotoOfTheDay() {
 		log.Fatal(err)
 	}
 
-	if absPath, err = filepath.Abs(usr.HomeDir + cacheDirname + tempFilename); err != nil {
+	if absPath, err = filepath.Abs(getAbsCacheDirname() + tempFilename); err != nil {
+		log.Fatal(err)
+	}
+
+	if err = exec.Command("gsettings", "set", "org.gnome.desktop.background", "picture-uri", absPath).Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func setRandomPhoto() {
+	var (
+		tempFilename = "/random_image.png"
+		absPath      string
+		err          error
+		file         *os.File
+		randomImage  []byte
+		unsplashAPI  UnsplashAPI
+	)
+
+	createCacheFolder()
+
+	randomImage = unsplashAPI.GetRandomImage()
+
+	if file, err = os.OpenFile(getAbsCacheDirname()+tempFilename, os.O_CREATE|os.O_RDWR, 0666); err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	if _, err = file.Write(randomImage); err != nil {
+		log.Fatal(err)
+	}
+
+	if absPath, err = filepath.Abs(getAbsCacheDirname() + tempFilename); err != nil {
 		log.Fatal(err)
 	}
 
