5c972a83d0fde5f67226140d05e87210ad47794d
Unknown <goncharovnikita.dev@gmail.com>
2017-12-31 16:06:13 +0300

    Move util functionality to separate file

---
+++ utils.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/user"
+)
+
+func createCacheFolder() {
+	var (
+		err error
+		usr *user.User
+	)
+	if usr, err = user.Current(); err != nil {
+		log.Fatal(err)
+	}
+
+	if err = os.Mkdir(usr.HomeDir+cacheDirname, 0777); err != nil {
+		var ok bool
+		if _, ok = err.(*os.PathError); !ok {
+			log.Fatal(err)
+		}
+	}
+}
+
+func getAbsCacheDirname() string {
+	var (
+		err error
+		usr *user.User
+	)
+	if usr, err = user.Current(); err != nil {
+		log.Fatal(err)
+	}
+
+	return usr.HomeDir + cacheDirname
+}
