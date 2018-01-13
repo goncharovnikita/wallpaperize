d1745648cdbc0a5b08cfd7864cfa0a1c0236b036
Unknown <goncharovnikita.dev@gmail.com>
2018-01-13 19:46:44 +0300

    Add reserve request to unsplash api

--- api/unsplash-api.go
+++ api/unsplash-api.go
@@ -12,6 +12,7 @@ var (
 	unsplashAPIprefix      = "https://api.unsplash.com/"
 	unsplashRandomPhotoURL = "photos/random"
 	unsplashAppID          = "288c71e3029fe7ff9572e518dfce06b383676fb0a7c1d8bc10cc3e06af252ed5"
+	unsplashAppIDBackup    = "af7ad21e753b2d1bac5e0d63d1ce9e1abefe5facb41c6252f1c741a45b64a658"
 )
 
 // UnsplashAPI implementation
@@ -51,7 +52,12 @@ func (u UnsplashAPI) GetRandomImage() (result []byte, err error) {
 	request.Header.Set("Authorization", "Client-ID "+unsplashAppID)
 
 	if response, err = client.Do(request); err != nil {
-		return
+		request.Header.Del("Authorization")
+		request.Header.Set("Authorization", "Client-ID "+unsplashAppIDBackup)
+		if response, err = client.Do(request); err != nil {
+			log.Print(err)
+			return
+		}
 	}
 
 	defer response.Body.Close()
@@ -61,6 +67,7 @@ func (u UnsplashAPI) GetRandomImage() (result []byte, err error) {
 	}
 
 	if err = json.Unmarshal(body, &data); err != nil {
+		log.Print(err)
 		return
 	}
 
@@ -70,21 +77,23 @@ func (u UnsplashAPI) GetRandomImage() (result []byte, err error) {
 	}
 
 	if response, err = http.Get(data.URLs.RAW); err != nil {
+		log.Print(err)
 		return
 	}
 
 	defer response.Body.Close()
 
 	if body, err = ioutil.ReadAll(response.Body); err != nil {
+		log.Print(err)
 		return
 	}
 
 	return body, nil
 }
 
-// GetRandomImageReader returns an Reader with
+// GetImageReader returns an Reader with
 // image entity
-func (u UnsplashAPI) GetRandomImageReader() (result io.ReadCloser, err error) {
+func (u UnsplashAPI) GetImageReader() (result io.ReadCloser, err error) {
 	var (
 		response *http.Response
 		data     unsplashRandomImageResponse
@@ -97,22 +106,40 @@ func (u UnsplashAPI) GetRandomImageReader() (result io.ReadCloser, err error) {
 	URL = unsplashAPIprefix + unsplashRandomPhotoURL + "?orientation=landscape&w=1920&h=1080"
 
 	if request, err = http.NewRequest(http.MethodGet, URL, nil); err != nil {
+		log.Print(err)
 		return
 	}
 
 	request.Header.Set("Authorization", "Client-ID "+unsplashAppID)
 
 	if response, err = client.Do(request); err != nil {
-		return
+		log.Print(err)
+		request.Header.Del("Authorization")
+		request.Header.Set("Authorization", "Client-ID "+unsplashAppIDBackup)
+		if response, err = client.Do(request); err != nil {
+			log.Print(err)
+			return
+		}
+	}
+
+	if response.StatusCode != http.StatusOK {
+		request.Header.Del("Authorization")
+		request.Header.Set("Authorization", "Client-ID "+unsplashAppIDBackup)
+		if response, err = client.Do(request); err != nil {
+			log.Print(err)
+			return
+		}
 	}
 
 	defer response.Body.Close()
 
 	if body, err = ioutil.ReadAll(response.Body); err != nil {
+		log.Print(err)
 		return
 	}
 
 	if err = json.Unmarshal(body, &data); err != nil {
+		log.Print(string(body), err)
 		return
 	}
 
@@ -122,6 +149,7 @@ func (u UnsplashAPI) GetRandomImageReader() (result io.ReadCloser, err error) {
 	}
 
 	if response, err = http.Get(data.URLs.RAW); err != nil {
+		log.Print(err)
 		return
 	}
 
