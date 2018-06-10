7ba825b6aa00e6a06354baffeca6947530cf2300
Nikita Goncharov <goncharovnikita.dev@gmail.com>
2018-06-10 20:04:20 +0300

    Add deploy app to travis

--- app/deploy.sh
+++ app/deploy.sh
@@ -13,7 +13,7 @@ do
     output_name=$GOOS'-'$GOARCH'-'$version
     env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name
     curl -F "data=@${output_name}" -L --header "BUILD_VERSION: ${output_name}" -i \
-        wallpaperize.goncharovnikita.com/add/build
+        https://wallpaperize.goncharovnikita.com/add/build
 
     if [ $? -ne 0 ]; then
         echo 'An error has occurred! Aborting the script execution...'
