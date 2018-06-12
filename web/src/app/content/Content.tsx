5806d79daaa57ddf42bae3026fe042ef3dee292e
Nikita Goncharov <goncharovnikita.dev@gmail.com>
2018-06-12 19:35:34 +0300

    Fix web build

--- web/src/app/content/Content.tsx
+++ web/src/app/content/Content.tsx
@@ -9,26 +9,32 @@ export class Content extends React.Component {
         super(props);
     }
 
+    content(): JSX.Element {
+        return (
+            <div className="jumbotron h-50">
+                <h1 className="display-1 text-center">Wallpaperize</h1>
+                <h1 className="font-weight-light text-center">{this.props.texts.about}</h1>
+                <hr className="my-4" />
+                <div className="row">
+                    <div className="col-md-2"></div>
+                    <div className="col-md-8">
+                        <SelectPlatform />
+                        <div className="alert alert-dark">
+                            <div className="code">
+                                <InstallCode />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-2"></div>
+                </div>
+            </div>
+        );
+    }
+
     render() {
         return(
             <div className="position-relative">
-                <div className="jumbotron h-50">
-                    <h1 className="display-1 text-center">Wallpaperize</h1>
-                    <h1 className="font-weight-light text-center">{this.props.texts.about}</h1>
-                    <hr className="my-4" />
-                    <div className="row">
-                        <div className="col-md-2"></div>
-                        <div className="col-md-8">
-                            <SelectPlatform />
-                            <div className="alert alert-dark">
-                                <div className="code">
-                                    <InstallCode />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-2"></div>
-                    </div>
-                </div>
+                {this.content()}
                 {/* <div className="overlay position-absolute h-100 w-100"></div> */}
             </div>
         );
