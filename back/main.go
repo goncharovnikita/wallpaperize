add1f71624f2f093f04f40c80dbd5275103705cb
Nikita Goncharov <goncharovnikita.work@gmail.com>
2020-05-23 17:16:38 +0300

    Add spec

--- back/main.go
+++ back/main.go
@@ -1,59 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 
 	"github.com/goncharovnikita/wallpaperize/app/api"
 	"github.com/goncharovnikita/wallpaperize/back/crawler"
 	"github.com/goncharovnikita/wallpaperize/back/server"
 	"github.com/goncharovnikita/wallpaperize/back/utils"
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
 )
 
+// Spec app specification
+type Spec struct {
+	Port                 int     `required:"false" default:"8080" envconfig:"PORT"`
+	BuildsPath           string  `required:"false" default:"uploads" envconfig:"BUILDS_PATH"`
+	RandomImagesPath     string  `required:"false" default:"random_images" envconfig:"RANDOM_IMAGES_PATH"`
+	MaxRandomDiskUsageGB float64 `required:"false" default:"1" envconfig:"MAX_RANDOM_DISK_USAGE_GB"`
+}
+
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
+	godotenv.Load()
+	spec := Spec{}
+	envconfig.MustProcess("", &spec)
 
-	path := os.Getenv("BUILDS_PATH")
-	if path == "" {
-		path = "uploads"
-		os.Mkdir(path, 0777)
-	}
-
-	randomImagesPath := os.Getenv("RANDOM_IMAGES_PATH")
-	if randomImagesPath == "" {
-		randomImagesPath = "random_images"
-		os.Mkdir(randomImagesPath, 0777)
-	}
+	os.Mkdir(spec.BuildsPath, 0777)
+	os.Mkdir(spec.RandomImagesPath, 0777)
 
 	maxRandomUsageInt := int64(0)
-	maxRandomUsage := os.Getenv("MAX_RANDOM_DISK_USAGE")
-	if maxRandomUsage == "" {
-		maxRandomUsageInt = utils.GetBytesFromGigabytes(1)
-	} else {
-		gbInt, err := strconv.ParseFloat(maxRandomUsage, 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if gbInt > 5 {
-			log.Fatal("we cannot afford that")
-		}
-		maxRandomUsageInt = utils.GetBytesFromGigabytes(gbInt)
+	if spec.MaxRandomDiskUsageGB > 5 {
+		log.Fatal("we cannot afford that much disk space")
 	}
+	maxRandomUsageInt = utils.GetBytesFromGigabytes(spec.MaxRandomDiskUsageGB)
 
-	rndCrawler := crawler.NewRandomCrawler(randomImagesPath, maxRandomUsageInt, api.UnsplashAPI{})
+	rndCrawler := crawler.NewRandomCrawler(spec.RandomImagesPath, maxRandomUsageInt, api.UnsplashAPI{})
 	go rndCrawler.Crawl()
 
-	minCrawer := crawler.NewMinCrawler([]string{randomImagesPath})
+	minCrawer := crawler.NewMinCrawler([]string{spec.RandomImagesPath})
 	go minCrawer.Crawl()
 
-	s := server.NewServer(path, randomImagesPath)
+	s := server.NewServer(spec.BuildsPath, spec.RandomImagesPath)
 	go s.Serve()
-	println("app listen on :: ", port)
+	println("app listen on :: ", spec.Port)
 
-	http.ListenAndServe(":"+port, nil)
+	http.ListenAndServe(fmt.Sprintf(":%d", spec.Port), nil)
 }
