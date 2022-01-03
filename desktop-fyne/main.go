bbb58e6dc1b849670a0d460386431add2bb15d59
Nikita Goncharov <goncharovnikita.dev@gmail.com>
2022-01-03 20:22:00 +0300

    init desktop-fyne project

---
+++ desktop-fyne/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	a := app.New()
+
+	w := a.NewWindow("Wallpaperize")
+	w.Resize(fyne.NewSize(480, 320))
+
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
+
+	w.ShowAndRun()
+}
